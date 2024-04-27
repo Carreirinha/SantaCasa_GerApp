5998085d99f87d3b6ec36094b6f35ea451ba35c5
Carreirinha <49102217+Carreirinha@users.noreply.github.com>
2024-04-27 18:48:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_pymongo import PyMongo
 from datetime import datetime
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/seu_banco_de_dados"
+app.config["MONGO_URI"] = "mongodb://mongo:27017/mydatabase"
 mongo = PyMongo(app)
 
 users_collection = mongo.db.users
